50be44045a8ce52769e634cb096a6443f30ddaad
Gan (Tony) YUAN <greybluesea@gmail.com>
2023-10-10 21:46:28 +1300

    updateHandler working

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/greybluesea/dockerised-fullstack-webapp-gofiber-gorm-postgres/database"
 	"github.com/greybluesea/dockerised-fullstack-webapp-gofiber-gorm-postgres/models"
@@ -12,6 +14,8 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/newfact", newfactHandler)
 	app.Post("/create", createHandler)
 	app.Post("/delete", deleteHandler)
+	app.Get("/edit/:id", editHandler)
+	app.Post("/update/:id", updateHandler)
 }
 
 func helloHandler(c *fiber.Ctx) error {
@@ -19,8 +23,13 @@ func helloHandler(c *fiber.Ctx) error {
 }
 func homeHandler(c *fiber.Ctx) error {
 	facts := []models.Fact{}
-	database.DB.Find(&facts)
+	result := database.DB.Find(&facts)
+	//	fmt.Println(reflect.TypeOf(facts[0].ID))
 	// 	return c.Status(200).JSON(facts)
+
+	fmt.Println(result)
+	fmt.Println(facts)
+
 	return c.Render("index", fiber.Map{"Title": "Fun Facts", "Subtitle": "Dockerised Fullstack WebApp(GoFiber + GORM + Postgres) - learned from Div Rhino", "Facts": facts})
 }
 
@@ -28,32 +37,69 @@ func newfactHandler(c *fiber.Ctx) error {
 	return c.Render("newfact", fiber.Map{"Title": "New Fact", "Subtitle": "Add an interesting fact"})
 }
 
-func createHandler(c *fiber.Ctx) error {
-	fact := new(models.Fact)
-	if err := c.BodyParser(fact); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": err.Error(),
-		})
-	}
-	database.DB.Create(&fact)
-	//return c.Status(200).JSON(fact)
-	return successHandler(c)
-}
+func editHandler(c *fiber.Ctx) error {
+	fact := &models.Fact{}
+	id := c.Params("id")
 
-func successHandler(c *fiber.Ctx) error {
-	return c.Render("success", fiber.Map{
-		"Title": "Fact added successfully",
+	result := database.DB.Where("id = ?", id).First(&fact)
+	if result.Error != nil {
+		return result.Error
+		//return NotFound(c)
+	}
+
+	return c.Render("edit", fiber.Map{
+		"Title": "Edit Fact",
+		"Fact":  fact,
 	})
 }
 
+func updateHandler(c *fiber.Ctx) error {
+	fact := &models.Fact{}
+	id := c.Params("id")
+
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusServiceUnavailable).SendString(err.Error())
+	}
+
+	result := database.DB.Model(fact).Where("id = ?", id).Updates(fact)
+	if result.Error != nil {
+		return editHandler(c)
+	}
+
+	return c.Redirect("/")
+}
+
+func createHandler(c *fiber.Ctx) error {
+	fact := &models.Fact{}
+	if err := c.BodyParser(fact); err != nil {
+		/* return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		}) */
+		return newfactHandler(c)
+	}
+	result := database.DB.Create(fact)
+	if result.Error != nil {
+		// return result.Error
+		return newfactHandler(c)
+	}
+	//return c.Status(200).JSON(fact)
+	// return  successHandler(c)
+	return c.Redirect("/")
+}
+
+/* func successHandler(c *fiber.Ctx) error {
+	return c.Render("success", fiber.Map{
+		"Title": "Fact added successfully",
+	})
+} */
+
 func deleteHandler(c *fiber.Ctx) error {
-	// Retrieve the fact ID from the POST request
 	factID := c.FormValue("ID")
+	// fmt.Println(reflect.TypeOf(factID))
 	result := database.DB.Delete(&models.Fact{}, factID)
 	if result.Error != nil {
 		return result.Error
 	}
-	// Redirect to the home page after deletion
 	c.Redirect("/")
 	return nil
 }
